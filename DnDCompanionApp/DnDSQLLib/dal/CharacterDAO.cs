37ce5480a5e40df578b399d1bfd197639f78a492
Mark Tov <mark@tovster.me>
2019-04-03 11:28:20 -0400

    Created the Character Deletion Method

--- DnDCompanionApp/DnDSQLLib/dal/CharacterDAO.cs
+++ DnDCompanionApp/DnDSQLLib/dal/CharacterDAO.cs
@@ -15,7 +15,7 @@ namespace DnDSQLLib.dal
 
         public CharacterDAO()
         {
-            
+
             try
             {
                 conn = ConnectionFactory.GetConnection();
@@ -37,13 +37,32 @@ namespace DnDSQLLib.dal
 
         public int deleteCharacter(int characterID)
         {
-            return 0;
+            int count = 0;
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand($"" +
+                    $"delete from character where Id = @chId");
+                cmd.Parameters.AddWithValue("@chId", characterID);
+                cmd.Connection = conn;
+
+                count = cmd.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                // Figure out how to deliver a good error message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return count;
         }
 
         public Character getCharacter(int characterID)
         {
             BackgroundDAO bgDAO = new BackgroundDAO();
-
+            Character character;
             int charID = 0;
             string name = "";
             int str = 0;
@@ -73,128 +92,138 @@ namespace DnDSQLLib.dal
             List<Skills> skills = new List<Skills>();
             List<Feature> features = new List<Feature>();
             List<Language> languages = new List<Language>();
-
-            conn.Open();
-            SqlCommand cmd = new SqlCommand($"select * from character where Id = @id");
-            cmd.Parameters.AddWithValue("@id", characterID);
-            cmd.Connection = conn;
-
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+            try
             {
-                charID  = Convert.ToInt32(reader["Id"]);
-                name = Convert.ToString(reader["Name"]);
-                str     = Convert.ToInt32(reader["Strength"]);
-                dex     = Convert.ToInt32(reader["Dexterity"]);
-                con     = Convert.ToInt32(reader["Constitution"]);
-                intel   = Convert.ToInt32(reader["Intelligence"]);
-                wis     = Convert.ToInt32(reader["Wisdom"]);
-                chr     = Convert.ToInt32(reader["Charisma"]);
-                strMod  = Convert.ToInt32(reader["strMod"]);
-                dexMod  = Convert.ToInt32(reader["dexMod"]);
-                conMod  = Convert.ToInt32(reader["conMod"]);
-                intMod  = Convert.ToInt32(reader["intMod"]);
-                wisMod  = Convert.ToInt32(reader["wisMod"]);
-                chrMod  = Convert.ToInt32(reader["chrMod"]);
-                hair = Convert.ToString(reader["Hair"]);
-                eyes = Convert.ToString(reader["Eyes"]);
-                skin = Convert.ToString(reader["Skin"]);
-                notes = Convert.ToString(reader["Notes"]);
-                raceID  = Convert.ToInt32(reader["RaceID"]);
-                classID = Convert.ToInt32(reader["ClassID"]);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand($"select * from character where Id = @id");
+                cmd.Parameters.AddWithValue("@id", characterID);
+                cmd.Connection = conn;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    charID = Convert.ToInt32(reader["Id"]);
+                    name = Convert.ToString(reader["Name"]);
+                    str = Convert.ToInt32(reader["Strength"]);
+                    dex = Convert.ToInt32(reader["Dexterity"]);
+                    con = Convert.ToInt32(reader["Constitution"]);
+                    intel = Convert.ToInt32(reader["Intelligence"]);
+                    wis = Convert.ToInt32(reader["Wisdom"]);
+                    chr = Convert.ToInt32(reader["Charisma"]);
+                    strMod = Convert.ToInt32(reader["strMod"]);
+                    dexMod = Convert.ToInt32(reader["dexMod"]);
+                    conMod = Convert.ToInt32(reader["conMod"]);
+                    intMod = Convert.ToInt32(reader["intMod"]);
+                    wisMod = Convert.ToInt32(reader["wisMod"]);
+                    chrMod = Convert.ToInt32(reader["chrMod"]);
+                    hair = Convert.ToString(reader["Hair"]);
+                    eyes = Convert.ToString(reader["Eyes"]);
+                    skin = Convert.ToString(reader["Skin"]);
+                    notes = Convert.ToString(reader["Notes"]);
+                    raceID = Convert.ToInt32(reader["RaceID"]);
+                    classID = Convert.ToInt32(reader["ClassID"]);
+                }
+                reader.Close();
+
+                // Getting Class Object
+                cmd = new SqlCommand($"" +
+                    $"select Name, Description, hitDice from class where Id = @cId");
+                cmd.Parameters.AddWithValue("@cId", classID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    charClass.Name = Convert.ToString(reader["Name"]);
+                    charClass.Description = Convert.ToString(reader["Description"]);
+                    charClass.HitDice = new Dice(Convert.ToInt32(reader["hitDice"]));
+                }
+                reader.Close();
+
+                // Getting Skills
+                cmd = new SqlCommand($"" +
+                    $"select skillId from classSkills where classId = @cId");
+                cmd.Parameters.AddWithValue("@cId", classID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int skillid = Convert.ToInt32(reader["skillId"]);
+                    skills.Add((Skills)skillid);
+                }
+                reader.Close();
+
+                // Get Features
+                cmd = new SqlCommand($"" +
+                    $"select f.Name, f.Description from features f" +
+                    $"join classFeatures cf on cf.FeatureId = f.Id" +
+                    $"join class c on cf.ClassId = c.Id" +
+                    $"where c.Id = @cId");
+                cmd.Parameters.AddWithValue("@cId", classID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    features.Add(new Feature(Convert.ToString(reader["Name"]), Convert.ToString(reader["Description"])));
+                }
+                reader.Close();
+
+                // Get Race
+                cmd = new SqlCommand($"" +
+                    $"select Name, Description from race where Id = @rId");
+                cmd.Parameters.AddWithValue("@rId", raceID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    raceName = Convert.ToString(reader["Name"]);
+                    raceDesc = Convert.ToString(reader["Description"]);
+                }
+                reader.Close();
+
+                // Languages
+                cmd = new SqlCommand($"" +
+                    $"select LanguageId from raceLanguage where raceId = @rId");
+                cmd.Parameters.AddWithValue("@rId", raceID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int languageid = Convert.ToInt32(reader["LanguageID"]);
+                    languages.Add((Language)languageid);
+                }
+                reader.Close();
+
+                race = new Race(raceName, raceDesc, languages);
+
+                // Getting Background
+                cmd = new SqlCommand($"" +
+                    $"select BackgroundId from character where Id = @chId");
+                cmd.Parameters.AddWithValue("@chId", characterID);
+                cmd.Connection = conn;
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    backgroundId = Convert.ToInt32(reader["BackgroundId"]);
+                }
+                bg = bgDAO.GetBackground(backgroundId);
+
+                character = new Character(name, str, dex, con, intel, wis, chr, strMod, dexMod, conMod, intMod, wisMod, chrMod, features, bg, hair, eyes, skin, notes, race, charClass);
             }
-            reader.Close();
-
-            // Getting Class Object
-            cmd = new SqlCommand($"" +
-                $"select Name, Description, hitDice from class where Id = @cId");
-            cmd.Parameters.AddWithValue("@cId", classID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while (reader.Read())
+            catch (SqlException)
             {
-                charClass.Name = Convert.ToString(reader["Name"]);
-                charClass.Description = Convert.ToString(reader["Description"]);
-                charClass.HitDice = new Dice(Convert.ToInt32(reader["hitDice"]));
+                // Figure out a good way to notify the users
+                character = null;
             }
-            reader.Close();
-
-            // Getting Skills
-            cmd = new SqlCommand($"" +
-                $"select skillId from classSkills where classId = @cId");
-            cmd.Parameters.AddWithValue("@cId", classID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while (reader.Read())
+            finally
             {
-                int skillid = Convert.ToInt32(reader["skillId"]);
-                skills.Add((Skills)skillid);
+                conn.Close();
             }
-            reader.Close();
-
-            // Get Features
-            cmd = new SqlCommand($"" +
-                $"select f.Name, f.Description from features f" +
-                $"join classFeatures cf on cf.FeatureId = f.Id" +
-                $"join class c on cf.ClassId = c.Id" +
-                $"where c.Id = @cId");
-            cmd.Parameters.AddWithValue("@cId", classID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while(reader.Read())
-            {
-                features.Add(new Feature(Convert.ToString(reader["Name"]), Convert.ToString(reader["Description"])));
-            }
-            reader.Close();
-
-            // Get Race
-            cmd = new SqlCommand($"" +
-                $"select Name, Description from race where Id = @rId");
-            cmd.Parameters.AddWithValue("@rId", raceID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                raceName = Convert.ToString(reader["Name"]);
-                raceDesc = Convert.ToString(reader["Description"]);
-            }
-            reader.Close();
-
-            // Languages
-            cmd = new SqlCommand($"" +
-                $"select LanguageId from raceLanguage where raceId = @rId");
-            cmd.Parameters.AddWithValue("@rId", raceID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                int languageid = Convert.ToInt32(reader["LanguageID"]);
-                languages.Add((Language)languageid);
-            }
-            reader.Close();
-
-            race = new Race(raceName, raceDesc, languages);
-
-            // Getting Background
-            cmd = new SqlCommand($"" +
-                $"select BackgroundId from character where Id = @chId");
-            cmd.Parameters.AddWithValue("@chId", characterID);
-            cmd.Connection = conn;
-
-            reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                backgroundId = Convert.ToInt32(reader["BackgroundId"]);
-            }
-            bg = bgDAO.GetBackground(backgroundId);
-
-            Character character = new Character(name, str, dex, con, intel, wis, chr, strMod, dexMod, conMod, intMod, wisMod, chrMod, features, bg, hair, eyes, skin, notes, race, charClass);
-
             return character;
         }
     }
