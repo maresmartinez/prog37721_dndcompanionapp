087066964adaed186ff0c43d6da0cddf0a4727f3
Mark Tov <mark@tovster.me>
2019-04-03 00:38:39 -0400

    Added a testing page for the DAOs in DBWebApp, completed UploadBackground, GetBackground. Next stop: Delete (like thats too hard)

--- DnDCompanionApp/DnDSQLLib/dal/BackgroundDAO.cs
+++ DnDCompanionApp/DnDSQLLib/dal/BackgroundDAO.cs
@@ -28,17 +28,81 @@ namespace DnDSQLLib.dal
 
         public int UploadBackground(Background bg)
         {
+            List<int> personalityId = new List<int>();
+            int idealId = 1; // Initializing with a valid value to prevent silliness
+            int bondId = 1;
+            int flawId = 1;
+            int count = 0;
+
             try
             {
                 conn.Open();
 
-                SqlCommand command = new SqlCommand();
+                // Getting Personality Ids
+                SqlCommand command = new SqlCommand($"" +
+                    $"select Id from personality where Description = @p1 OR Description = @p2");
+                command.Parameters.AddWithValue("@p1", bg.Personality[0]);
+                command.Parameters.AddWithValue("@p2", bg.Personality[1]);
+                command.Connection = conn;
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                    personalityId.Add(Convert.ToInt32(reader["Id"]));
+                reader.Close();
+
+                // Getting IdealId
+                command = new SqlCommand($"" +
+                    $"Select Id from ideal where Description = @ideal");
+                command.Parameters.AddWithValue("@ideal", bg.Ideals[0]);
+                command.Connection = conn;
+
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                    idealId = Convert.ToInt32(reader["Id"]);
+                reader.Close();
+
+                // Getting BondId
+                command = new SqlCommand($"" +
+                    $"Select Id from bond where Description = @bond");
+                command.Parameters.AddWithValue("@bond", bg.Bonds[0]);
+                command.Connection = conn;
+
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                    bondId = Convert.ToInt32(reader["Id"]);
+                reader.Close();
+
+                //Getting FlawId
+                command = new SqlCommand($"" +
+                    $"Select Id from flaw where Description = @flaw");
+                command.Parameters.AddWithValue("@flaw", bg.Flaws[0]);
+                command.Connection = conn;
+
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                    flawId = Convert.ToInt32(reader["Id"]);
+                reader.Close();
+
+                // Putting it all together for actually submitting it to the DB
+                command = new SqlCommand($"" +
+                    $"Insert into background (PersonalityID1, PersonalityID2, IdealID, BondID, FlawID) values (@p1, @p2, @i, @b, @f)");
+                command.Parameters.AddWithValue("@p1", personalityId[0]);
+                command.Parameters.AddWithValue("@p2", personalityId[1]);
+                command.Parameters.AddWithValue("@i", idealId);
+                command.Parameters.AddWithValue("@b", bondId);
+                command.Parameters.AddWithValue("@f", flawId);
+
+                count = command.ExecuteNonQuery();
             }
             catch (SqlException)
             {
 
             }
-            return 0;
+            finally
+            {
+                conn.Close();
+            }
+            return count;
         }
         public int DeleteBackground(int backgroundId)
         {
@@ -103,17 +167,17 @@ namespace DnDSQLLib.dal
                 background = new Background(backgroundId, personality, ideal, bond, flaw);
 
             }
-            catch (SqlException s)
+            catch (SqlException)
             {
                 // Figure out what to give with the exception
                 background = null;
             }
+            finally
+            {
+                conn.Close();
+            }
 
             return background;
-        }
-        public int UpdateBackground(int backgroundId)
-        {
-            return 0;
         }
     }
 }
---
+++ DnDCompanionApp/DnDWebApp/Testing.aspx.cs
@@ -0,0 +1,23 @@
+﻿using CharacterCreationLib;
+using DnDSQLLib.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DnDWebApp
+{
+    public partial class Testing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BackgroundDAO bdao = new BackgroundDAO();
+            Background background = bdao.GetBackground(1);
+
+            Label1.Text = background.OtherToString();
+            
+        }
+    }
+}
