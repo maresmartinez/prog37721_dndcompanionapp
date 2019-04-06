c68a64f5bf41bf7b2aac567fd233b73b5bc36899
Mariel Martinez <mcortez.martinez@gmail.com>
2019-04-06 13:35:37 -0400

    Added a few TODOs to the tasklist and updated .dacpac with latest database schema+data

--- DnDCompanionApp/DnDSQLLib/dal/ConnectionFactory.cs
+++ DnDCompanionApp/DnDSQLLib/dal/ConnectionFactory.cs
@@ -6,18 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DnDSQLLib.dal {
+    /// <summary>
+    /// Centralizes the database connection creation
+    /// </summary>
     class ConnectionFactory {
-        // TODO: remove MARS from production build
-        public static string server1 = @"Data Source=HAM\SQLEXPRESS;Initial Catalog=DungeonsAndDragonsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=True";
-        public static string server2 = "";  // Used in the event of redundancy
-        public static SqlConnection GetConnection() {
-            return new SqlConnection(server1);
 
-            //try {
-            //    return new SqlConnection(server1);
-            //} catch (SqlException) {
-            //    return new SqlConnection(server2);  // Flips over to the redundant server is things go horribly wrong with the Azure instance
-            //}
+        /// <summary>
+        /// This will be the connection string for our Azure serer instance
+        /// TODO: replace local server string with Azure server connectio string once it's up
+        /// </summary>
+        public static string server1 = @"Data Source=HAM\SQLEXPRESS;Initial Catalog=DungeonsAndDragonsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// The connection string for a local server to provide redundancy in case of failure of the Azure server
+        /// </summary>
+        public static string server2 = "";  // Used in the event of server1 failure
+
+        /// <summary>
+        /// Insantiates an SqlConnection with either the Azure server, or the backup localhost server
+        /// </summary>
+        /// <returns>A connection to the DnD database</returns>
+        public static SqlConnection GetConnection() {
+            try {
+                // TODO: remove Multiple Active Result Sets from production build; we're going to refactor our code so we don't need it
+                return new SqlConnection(server1 + ";MultipleActiveResultSets=True");
+            } catch (SqlException) {
+                return new SqlConnection(server2 + ";MultipleActiveResultSets=True"); 
+            }
         }
     }
 }
